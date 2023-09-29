8c0256a22fd92914bae028a1333d8c20867c72c6
Thakur Abhay <zzxxcc6386mail.com>
2023-09-29 18:17:48 +0530

    javascript

--- 02_datatypes/datatype.js
+++ 02_datatypes/datatype.js
@@ -1,10 +1,12 @@
+/*
+
 "use strict" ; //treat all js code as newer version it is bydefault
 
 alert(3+3);
 
 console.error("anything we can write");
 
-
+*/
 
 
 
@@ -19,3 +21,48 @@ console.error("anything we can write");
 
 
 
+// non premitive datatype
+// object , array , function
+
+
+
+
+/*
+
+
+
+// Big Int
+
+const biginteger = 8798455098365349867
+const bigintege = 8798455098365349867n
+console.log(biginteger);
+console.log(typeof (biginteger))
+console.log(typeof (bigintege))
+
+id = Symbol('123')
+id2 = Symbol('123')
+console.log(id==id2) //false because symbolw always store unique veriables
+console.log(id)
+
+arry1 = ["abhay" , "ajit " , "thakur" , "mitisha"]
+
+
+// object
+let obj = {
+    name : "abhay",
+    middle : "pratap",
+}
+
+
+
+*/
+
+
+
+//function 
+const funs = function(){
+    console.log("this is a print by function");
+};
+
+
+console.log(funs());
--- 03_conversion_and_operattion/conversion_and_operattion.js
+++ 03_conversion_and_operattion/conversion_and_operattion.js
@@ -1,3 +1,6 @@
+// ***************************   conversion    ******************
+
+
 /*
 console.log("abhay");
 console.error("conversion_and_operattion")
@@ -29,6 +32,8 @@ console.log(typeof (changebool));  // 0/1
 console.log(changebool)
 */
 
+
+/*
 console.log(typeof null);
 console.log(typeof undefined);
 
@@ -46,3 +51,23 @@ strnum = "32er"
 console.log(typeof ("strnum")); //it is a string we chage in number
 strnumchange = Number (strnum);
 console.log(strnumchange);   //NaN
+
+*/
+
+
+
+// *****************     Operration   ******************
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%3);
+console.log(2%5);
+console.log(3%2);
+
+*/
+
+
