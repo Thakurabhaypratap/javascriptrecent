04681c66d5b74569dea3b36e9c4f9cd9dd2dddf4
Thakur Abhay Pratap Singh <117711105+Thakurabhaypratap@users.noreply.github.com>
2023-10-04 20:41:47 +0000

    javascript

--- test.js
+++ test.js
@@ -1,2 +1,28 @@
 console.log("Thakur Abhay")
 console.log("Thakur Abhay")
+
+
+
+
+
+const aaa = function(){
+    id = 1,
+    fullname={
+        first:"abhay",
+        title:"singh"
+    };
+
+    let fun2 = function(){
+        var nam ="abhay"
+        let tit ="singh"
+        var fun3 = function(){
+            document.write("this is fun 3")
+        }
+    };
+    document.write(fun3)
+
+       
+
+}
+
+
