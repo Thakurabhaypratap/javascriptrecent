7ea4bc2685bab96f764a2d793ecc38a18c818ca6
Thakur Abhay <zzxxcc6386mail.com>
2023-10-05 19:27:19 +0530

    arrow

--- 12_this_and_arrow_function/arrow.js
+++ 12_this_and_arrow_function/arrow.js
@@ -1,5 +1,9 @@
 const arr1 = (num1 ,num2) => {
     return(num1 + num2)
 }
-console.log(arr1 (54 ,64))
+console.log(arr1 (54 ,64));
+const addTwo = (num1 , num2) => num1 + num2
+console.log(addTwo(3434 ,353));
+
+
 
