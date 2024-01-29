0ce15b7359cdcf5124ddba8f61eca0eb7a1640ab
abhay <zzxxcc6386@gmail.com>
2024-01-29 23:57:43 +0530

    prototype

--- 31.5_constroctor()/index.js
+++ 31.5_constroctor()/index.js
@@ -1,8 +1,22 @@
 class railwayfield {
-    constructor(){
-        console.log(`constructor allwaves will be called autometicaly  when we we will be crate a object     :  name is ${this.name1} and train no ${this.trainno}` );
-        this.name1 = name1;
+    constructor(name1, trainno){
+
+
+        console.log(`constructor allwaves will be called autometicaly  when we we will be crate a object     :  name is ${name1} and train no ${trainno}` )
+
+
+
         this.trainno = trainno;
+        this.name1 = name1;
+
+
+
+
+
+        console.log(`constructor allwaves will be called autometicaly  when we we will be crate a object     :  name is ${this.name1} and train no ${this.trainno}` )
+
+
+
     }
     submit(){
         console.log(this.name1 + ' submitted with train no ' + this.trainno );
@@ -16,6 +30,26 @@ class railwayfield {
 let abhay = new railwayfield('abhay' , 563557);
 abhay.cancle()
 abhay.submit()
+
+
+
+// class railwayfield {
+//     constructor(name1, trainno) {
+//         this.name1 = name1;
+//         this.trainno = trainno;
+//         console.log(`constructor allwaves will be called automatically when we create an object: name is ${this.name1} and train no ${this.trainno}`);
+//     }
+//     submit() {
+//         console.log(`${this.name1} submitted with train no ${this.trainno}`);
+//     }
+//     cancel() {
+//         console.log(`${this.name1} canceled with train no ${this.trainno}`);
+//     }
+// }
+
+// let abhay = new railwayfield('abhay', 563557);
+// abhay.cancel();
+// abhay.submit();
 
 
 
