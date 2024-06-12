be415e3e5c2236e2d131a83d29d5e2c06fbe20f5
Thakur Abhay Pratap Singh <117711105+Thakurabhaypratap@users.noreply.github.com>
2024-06-13 01:01:24 +0530

    Update string.js

--- 05_data_type_advance_modern_str/string.js
+++ 05_data_type_advance_modern_str/string.js
@@ -5,7 +5,7 @@ document.write(a1 + a2  + " singh")
 console.log(a1 + a2 + "singh");
 
 // modern syntex 
-// in modern day we need to go thrue with Backticks in this mathod we used string manuplation
+// in modern day we need to go thrue with Backticks in this mathod we used template literals.
 console.log(`hello my name is ${a1} and title is ${a2}`)
 
 const gamename = new String('anystring')
